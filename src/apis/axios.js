a9eb147e88e8bc7424c75ca1aca785986f44d577
pras306 <44067654+pras306@users.noreply.github.com>
2021-09-08 01:07:19 +0530

    Removed unwanted files

--- src/apis/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const UNSPLASH_API = axios.create({
-    baseURL: "https://api.unsplash.com",
-    headers: {
-        Authorization : `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEY}`
-    }
-});
